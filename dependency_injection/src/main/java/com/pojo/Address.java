278e48400c1d4cdf0b45bbe1c0b2d420d869b8e3
Weikang01 <lwkdota@qq.com>
2021-07-23 22:24:49 -0400

    After Dependency Injection (DI)

---
+++ dependency_injection/src/main/java/com/pojo/Address.java
@@ -0,0 +1,16 @@
+package com.pojo;
+
+public class Address {
+    private String address;
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "address='" + address + '\'' +
+                '}';
+    }
+}
---
+++ dependency_injection/src/test/java/MyTest.java
@@ -0,0 +1,11 @@
+import com.pojo.Student;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MyTest {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("beams.xml");
+        Student student = (Student) context.getBean("student");
+        System.out.println(student);
+    }
+}
