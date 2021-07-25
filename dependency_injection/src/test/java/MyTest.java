e933ea3b0c7ebe24468dcb6b95d22780b1e6b9fc
Weikang01 <lwkdota@qq.com>
2021-07-24 21:08:57 -0400

    After Bean Scopes

--- dependency_injection/src/test/java/MyTest.java
+++ dependency_injection/src/test/java/MyTest.java
@@ -1,4 +1,6 @@
 import com.pojo.Student;
+import com.pojo.User;
+import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -7,5 +9,22 @@ public class MyTest {
         ApplicationContext context = new ClassPathXmlApplicationContext("beams.xml");
         Student student = (Student) context.getBean("student");
         System.out.println(student);
+    }
+
+    @Test
+    public void test()
+    {
+        ApplicationContext context = new ClassPathXmlApplicationContext("userBeams.xml");
+        User user = (User) context.getBean("user");
+        User user2 = (User) context.getBean("user");
+
+        /*
+        * by default bean's scope is "singleton"
+        * true if scope="singleton": Spring will return the unique object every time we call it
+        * false if scope="prototype": Spring will generate a new object every time we get bean
+        * scope="request","session","application" only work in web development
+        * */
+        System.out.println(user == user2);
+
     }
 }
