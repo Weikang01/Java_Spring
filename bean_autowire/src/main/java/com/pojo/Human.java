5c435fff2c5d48b64f7c34105d87e30d7e965e78
Weikang01 <lwkdota@qq.com>
2021-07-25 02:16:38 -0400

    After AutoWire with Annotation

--- bean_autowire/src/main/java/com/pojo/Human.java
+++ bean_autowire/src/main/java/com/pojo/Human.java
@@ -1,33 +1,43 @@
 package com.pojo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import javax.annotation.Resource;
+
 public class Human {
+    @Resource(name = "cat111") // Resource: search bean by name and by type
     private Cat cat;
+    // Autowired: search bean by name
+    @Autowired(required = false)  // required = true == variable is not nullable
+    @Qualifier(value = "dog111")
     private Dog dog;
     private String name;
+
 
     public Cat getCat() {
         return cat;
     }
 
-    public void setCat(Cat cat) {
-        this.cat = cat;
-    }
+//    public void setCat(Cat cat) {
+//        this.cat = cat;
+//    }
 
     public Dog getDog() {
         return dog;
     }
 
-    public void setDog(Dog dog) {
-        this.dog = dog;
-    }
+//    public void setDog(Dog dog) {
+//        this.dog = dog;
+//    }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+//    public void setName(String name) {
+//        this.name = name;
+//    }
 
     @Override
     public String toString() {
